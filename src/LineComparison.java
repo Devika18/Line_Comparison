e838d79032856c9acfc1f3e8e1040267153642a6
Devika18 <devikankarpe@gmail.com>
2022-10-06 23:09:24 +0530

    added: logic for comparing two lengths

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -7,10 +7,16 @@ public class LineComparison {
         Integer x2 = 6;
         Integer y2 = 8;
         double lengthOfLine = Math.sqrt((x2-x1)^2 + (y2-y1)^2);
+        System.out.println("Length of Line is: "+lengthOfLine);
 
         boolean result1 = x1.equals(y1);
         System.out.println("Result of equals()="+result1);
         boolean result2 = x2.equals(y2);
         System.out.println("Result of equals()="+result2);
+
+        int compareLength1 = Integer.compare(x1,y1);
+        System.out.println("Result of compare(x1,y1)is:"+ compareLength1);
+        int compareLength2 = Integer.compare(x2,y2);
+        System.out.println("Result of compare(x2,y2)is:"+ compareLength2);
     }
 }
