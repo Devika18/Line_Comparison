41e7e4a0dd08a82bf438fa5ff402b25b3ae8c97a
Devika18 <devikankarpe@gmail.com>
2022-10-06 23:05:40 +0530

    added: logic for checking equality

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -2,11 +2,15 @@ import java.util.Random;
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Program");
-        int x1 = 9;
-        int y1 = 5;
-        int x2 = 6;
-        int y2 = 8;
-        double length = Math.sqrt((x2-x1)^2 + (y2-y1)^2);
-        System.out.println("Length Of Line is:"+length);
+        Integer x1 = 9;
+        Integer y1 = 5;
+        Integer x2 = 6;
+        Integer y2 = 8;
+        double lengthOfLine = Math.sqrt((x2-x1)^2 + (y2-y1)^2);
+
+        boolean result1 = x1.equals(y1);
+        System.out.println("Result of equals()="+result1);
+        boolean result2 = x2.equals(y2);
+        System.out.println("Result of equals()="+result2);
     }
 }
